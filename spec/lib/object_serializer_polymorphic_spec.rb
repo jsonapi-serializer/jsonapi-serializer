0a13a653a3ffc4283b4dd3262facc3f051dfe212
Thomas <ts@tcare.fr>
2019-10-05 14:55:59 +0200

    add tests

--- spec/lib/object_serializer_polymorphic_spec.rb
+++ spec/lib/object_serializer_polymorphic_spec.rb
@@ -7,10 +7,22 @@ describe FastJsonapi::ObjectSerializer do
 
   class ChecklistItem
     attr_accessor :id, :name
+
+    # fake belongs_to :list
+    attr_accessor :list_id
+    def list
+      []
+    end
   end
 
   class Car
     attr_accessor :id, :model, :year
+
+    # fake belongs_to :list
+    attr_accessor :list_id
+    def list
+      []
+    end
   end
 
   class ListSerializer
@@ -19,6 +31,21 @@ describe FastJsonapi::ObjectSerializer do
     attributes :name
     set_key_transform :dash
     has_many :items, polymorphic: true
+  end
+
+  class ChecklistItemSerializer
+    include FastJsonapi::ObjectSerializer
+    set_type :checklist_item
+    attributes :name
+    belongs_to :list
+  end
+
+  class CarSerializer
+    include FastJsonapi::ObjectSerializer
+    set_type :car
+    attributes :model
+    attributes :year
+    belongs_to :list
   end
 
   let(:car) do
@@ -46,6 +73,15 @@ describe FastJsonapi::ObjectSerializer do
       expect(record_type).to eq 'checklist-item'.to_sym
       record_type = list_hash[:data][:relationships][:items][:data][1][:type]
       expect(record_type).to eq 'car'.to_sym
+    end
+  end
+
+  context 'when serializing included polymorphic relationships' do
+    it "should not raise" do
+      list = List.new
+      list.id = 1
+      list.items = [checklist_item, car]
+      expect(-> {ListSerializer.new(list, include: ["items.list"])}).not_to raise_error
     end
   end
 end
