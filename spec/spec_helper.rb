df95cbda32feb57e2fead8a5531b96e16cea56c6
Stas SUȘCOV <stas@nerd.ro>
2020-03-01 18:57:38 +0000

    Remove performance and skipped tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,6 @@
 require 'active_record'
 require 'fast_jsonapi'
-require 'rspec-benchmark'
 require 'byebug'
-require 'active_model_serializers'
-require 'oj'
-require 'jsonapi/serializable'
-require 'jsonapi-serializers'
 
 Dir[File.dirname(__FILE__) + '/shared/contexts/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/shared/examples/*.rb'].each {|file| require file }
-
-RSpec.configure do |config|
-  config.include RSpec::Benchmark::Matchers
-  config.filter_run_excluding performance: ENV['BENCHMARK'].blank?
-end
-
-Oj.optimize_rails
-ActiveModel::Serializer.config.adapter = :json_api
-ActiveModel::Serializer.config.key_transform = :underscore
-ActiveModelSerializers.logger = ActiveSupport::TaggedLogging.new(ActiveSupport::Logger.new('/dev/null'))
