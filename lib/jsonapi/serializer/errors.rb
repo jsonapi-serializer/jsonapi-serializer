f8255771dc80225bb87ecdb259ac1bd15a831d0a
nattfodd <nattfodd.pp.ua@gmail.com>
2020-09-17 15:03:49 +0300

    Raise FastJsonapi scoped error in case of unsupported include

---
+++ lib/jsonapi/serializer/errors.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module JSONAPI
+  module Serializer
+    class Error < StandardError; end
+    class UnsupportedIncludeError < Error
+      attr_reader :include_item, :klass
+
+      def initialize(include_item, klass)
+        @include_item = include_item
+        @klass = klass
+      end
+
+      def message
+        "#{include_item} is not specified as a relationship on #{klass}"
+      end
+    end
+  end
+end
