1f6a2b4d3610baa742d4893d4d58a344009fcaa3
Bruno Arueira <contato@brunoarueira.com>
2022-01-07 13:31:27 -0300

    Fixes test suite

--- lib/fast_jsonapi/object_serializer.rb
+++ lib/fast_jsonapi/object_serializer.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'active_support/isolated_execution_state'
 require 'active_support/time'
 require 'active_support/concern'
 require 'active_support/inflector'
