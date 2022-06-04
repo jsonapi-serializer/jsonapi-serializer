dc874a0034161b5d740eabcfb4d686d0fa36a194
rkwap <ramitkaul@gmail.com>
2022-06-04 20:43:52 +0530

    added initial changes

--- lib/fast_jsonapi/scalar.rb
+++ lib/fast_jsonapi/scalar.rb
@@ -1,15 +1,16 @@
 module FastJsonapi
   class Scalar
-    attr_reader :key, :method, :conditional_proc
+    attr_reader :key, :method, :conditional_proc, :optional
 
     def initialize(key:, method:, options: {})
       @key = key
       @method = method
       @conditional_proc = options[:if]
+      @optional = options[:optional] || false
     end
 
-    def serialize(record, serialization_params, output_hash)
-      if conditionally_allowed?(record, serialization_params)
+    def serialize(record, serialization_params, output_hash, allowed_optionals)
+      if conditionally_allowed?(record, serialization_params, allowed_optionals)
         if method.is_a?(Proc)
           output_hash[key] = FastJsonapi.call_proc(method, record, serialization_params)
         else
@@ -18,7 +19,9 @@ module FastJsonapi
       end
     end
 
-    def conditionally_allowed?(record, serialization_params)
+    def conditionally_allowed?(record, serialization_params, allowed_optionals)
+      return false if optional.present? && !allowed_optionals.include?(@key)
+
       if conditional_proc.present?
         FastJsonapi.call_proc(conditional_proc, record, serialization_params)
       else
