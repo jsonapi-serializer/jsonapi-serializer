60d54f60ded98089f090f9fa5487177f2d32b4cb
Oliver Höhn <oliver@thriva.co>
2023-01-27 17:09:19 +0000

    Prevent undefined method errors when include is provided
    What?
    - Default to an empty hash when fetching relationships for a serializer
      class that does not have any relationships defined.
    
    Why?
    Previously, if `include` is passed in as argument for a serializer
    that does not have relationships defined, then a: `undefined method []
    for nil:NilClass` is raised rather than the expected
    `JSONAPI::Serializer::UnsupportedIncludeError`. This was because
    `Serializer#relationships_to_serialize` was returning `nil`.
    
    Refs
    - https://github.com/jsonapi-serializer/jsonapi-serializer/issues/210

--- spec/integration/errors_spec.rb
+++ spec/integration/errors_spec.rb
@@ -21,5 +21,16 @@ RSpec.describe JSONAPI::Serializer do
           JSONAPI::Serializer::UnsupportedIncludeError, /bad_include is not specified as a relationship/
         )
     end
+
+    context 'when include is provided for a resource that does not have relationships' do
+      let(:user) { User.fake }
+
+      it do
+        expect { UserSerializer.new(user, include: ['bad_include']) }
+          .to raise_error(
+            JSONAPI::Serializer::UnsupportedIncludeError, /bad_include is not specified as a relationship/
+          )
+      end
+    end
   end
 end
